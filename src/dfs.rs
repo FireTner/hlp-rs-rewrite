53f3084779caf1d593e9e8150e4cf19af2646c87
FireTner <vladik.cerny.2007@seznam.cz>
2023-12-02 01:27:03 +0100

    Improved comments for dfs

--- src/dfs.rs
+++ src/dfs.rs
@@ -1,8 +1,13 @@
-// use crate::layer::Layer;
 use crate::vec::i8x16;
 use crate::tables::Tables;
 use crate::applylayer::apply_layer;
 
+// Depth first search function
+// Takes depth, output of previous function, previous index, and `tables` as parameters
+// Returns true if it found the correct solution otherwise false
+//
+// This function either calls itself recursively until it depth is same as tables.current_layer
+// then it calls last_layer to check if it found a solution
 pub fn dfs(depth: &usize, input: &i8x16, prev_index: &usize, tables: &Tables) -> bool {
   if *depth == tables.cur_layer { return last_layer(&input, &prev_index, &tables); }
 
@@ -16,6 +21,11 @@ pub fn dfs(depth: &usize, input: &i8x16, prev_index: &usize, tables: &Tables) ->
   false
 }
 
+// Last layer search function
+// Takes output of previous function, previous index, and `tables` as parameters
+// Returns true if it found the correct solution otherwise false
+//
+// Loops over possible layers and returns true if solution was found
 pub fn last_layer(input: &i8x16, prev_index: &usize, tables: &Tables) -> bool {
   for i in 0..tables.pairs[*prev_index].len() {
     let index = tables.pairs[*prev_index][i];
@@ -27,6 +37,11 @@ pub fn last_layer(input: &i8x16, prev_index: &usize, tables: &Tables) -> bool {
   return false;
 }
 
+// First layer search function
+// Takes `tables` as parameters
+// Returns true if found a solution otherwise false
+//
+// Calls dfs and recursively tries to bruteforce a solution
 pub fn first_layer(tables: &Tables) -> bool {
   if tables.cur_layer == 0 { return false; }
 
