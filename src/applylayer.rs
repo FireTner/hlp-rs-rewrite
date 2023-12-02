29eaf52f258fb2f154fb16c05db4a59316e2d55d
FireTner <vladik.cerny.2007@seznam.cz>
2023-12-02 01:28:57 +0100

    Improved comments for applylayer

--- src/applylayer.rs
+++ src/applylayer.rs
@@ -1,5 +1,8 @@
 use crate::vec::i8x16;
 
+// Applies transmutation to a layer
+// Takes input to transmute, mask to transmute by, and goal as parameters
+// Returns the transmuted i8x16
 pub fn apply_layer(input: &i8x16, mask: &i8x16, _goal: &i8x16) -> i8x16 {
   return mask.shuffle(input);
 }
