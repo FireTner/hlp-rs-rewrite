1dda7d9a9a4c8f1dfaa4fdc2f8406fb0a88d4a19
FireTner <vladik.cerny.2007@seznam.cz>
2023-12-02 01:45:18 +0100

    Improved readability for lut

--- src/lut.rs
+++ src/lut.rs
@@ -1,16 +1,21 @@
-use crate::vec::i8x16;
 use crate::unicount::unicount;
+use crate::vec::i8x16;
 use crate::layer::*;
 
+// Generates a look up table
+// Takes goal as a parameter  
+// Returns a vector look up table
+//
+// Generates a table of all possible unique 1 layer configurations
 pub fn gen_lut(goal: i8x16) -> Vec<Layer> {
-  let start: i8x16 = i8x16::start();
   let mut layers: Vec<Layer> = Vec::new();
   let goal_uc = unicount(goal);
 
   for configuration in 0..CONFCOUNT {
-    let output = Layer::generate(start, configuration);
-
-    if output.layer == start { continue; }
+    let output = Layer::generate(i8x16::start(), configuration);
+    
+    // Removes identity function, invalid, illegal and non-unique layers
+    if output.layer == i8x16::start() { continue; }
     if output.layer == i8x16::zero() { continue; }
     if unicount(output.layer) < goal_uc { continue; }
     if layers.contains(&output) { continue; }
